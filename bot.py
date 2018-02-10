cb619143e24c20801ae2dcaa9c48710c63c60712
Adam Olgrzymek <mrasminaf@gmail.com>
2018-02-10 18:11:57 +0100

    Added bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from cfg import *
+import socket
+import re
+
+def send_message(message):
+    s.send(bytes("PRIVMSG #" + NICK + " :" + message + "\r\n", "UTF-8"))
+
+s = socket.socket()
+s.connect((HOST, PORT))
+s.send(bytes("PASS " + PASS + "\r\n", "UTF-8"))
+s.send(bytes("NICK " + NICK + "\r\n", "UTF-8"))
+s.send(bytes("JOIN #" + NICK + " \r\n", "UTF-8"))
+
+send_message("Bot has just been turned on")
+
+CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+while True:
+    response = s.recv(1024).decode("utf-8")
+    if response == "PING :tmi.twitch.tv\r\n":
+        s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+    else:
+        username = re.search(r"\w+", response).group(0)
+        message = CHAT_MSG.sub("", response)
+        print(response)
+
+        if message.strip() == "!info":
+            send_message("My name is Adam. I'm 16 and I'm from Poland")
